269d4a8305729f637e1bfeaa41a25949ef73601b
Davide <davide.lombardi2@stud.unifi.it>
2023-05-24 17:36:23 +0200

    +++

--- blog/urls.py
+++ blog/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
 from . import views
+from .views import AddPost
 
 urlpatterns = [
     path('', views.blog, name= 'home'),
+    path('<slug:slug>/', views.PostDetail.as_view(), name='post_detail'),
     path('contact', views.contact, name='contact'),
+    path('add_post', AddPost.as_view()),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.views import generic
 from .models import Post
 
 
@@ -11,3 +12,19 @@ def blog(request):
 
 def contact(request):
     return render(request, 'contact.html')
+
+
+class PostList(generic.ListView):
+    queryset = Post.objects.filter(status=1).order_by('-created_on')
+    template_name = 'post_list.html'
+
+
+class PostDetail(generic.DetailView):
+    model = Post
+    template_name = 'post_detail.html'
+
+
+class AddPost(generic.CreateView):
+    model = Post
+    template_name = 'add_post.html'
+    fields = '__all__'
