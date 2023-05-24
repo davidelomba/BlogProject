3746c16042d712598ab9111a46aab5a877a80a00
Davide <davide.lombardi2@stud.unifi.it>
2023-05-24 18:37:01 +0200

    add the creation of post

--- blog/urls.py
+++ blog/urls.py
@@ -1,10 +1,10 @@
 from django.urls import path
 from . import views
-from .views import AddPost
+from .views import AddPost, PostDetail
 
 urlpatterns = [
     path('', views.blog, name= 'home'),
-    path('<slug:slug>/', views.PostDetail.as_view(), name='post_detail'),
+    path('post_detail/<int:pk>', PostDetail.as_view(), name='post_detail'),
     path('contact', views.contact, name='contact'),
-    path('add_post', AddPost.as_view()),
+    path('add_post', AddPost.as_view(), name= 'add_post'),
 ]
