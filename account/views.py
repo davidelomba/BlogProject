45dff81f81f712074d56680a080d64874196c0cb
Davide <davide.lombardi2@stud.unifi.it>
2023-05-26 16:38:46 +0200

    add user registration and login

---
+++ account/views.py
@@ -0,0 +1,13 @@
+from django.shortcuts import render
+from django.views import generic
+from django.contrib.auth.forms import UserCreationForm
+from django.urls import reverse_lazy
+from django.views.generic import CreateView
+
+
+class UserRegistration(CreateView):
+    form_class = UserCreationForm
+    template_name = 'registration/register.html'
+    success_url = reverse_lazy('login')
+
+
