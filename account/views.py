d4a18b5dd1827823dd7182f4081645d642b2833d
Davide <davide.lombardi2@stud.unifi.it>
2023-06-01 16:46:18 +0200

    add First Name, Last Name and Email in Registration Form

--- account/views.py
+++ account/views.py
@@ -4,9 +4,11 @@ from django.contrib.auth.forms import UserCreationForm
 from django.urls import reverse_lazy
 from django.views.generic import CreateView
 
+from account.forms import SignUpForm
+
 
 class UserRegistration(CreateView):
-    form_class = UserCreationForm
+    form_class = SignUpForm
     template_name = 'registration/register.html'
     success_url = reverse_lazy('login')
 
