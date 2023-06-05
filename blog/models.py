2d3e3fd941bef06e0d4891790841330cac44aaaa
Davide <davide.lombardi2@stud.unifi.it>
2023-06-05 11:14:03 +0200

    Add Comment

--- blog/models.py
+++ blog/models.py
@@ -67,3 +67,13 @@ class Category(models.Model):
 
     def get_absolute_url(self):
         return reverse('home')
+
+
+class Comment(models.Model):
+    post = models.ForeignKey(Post, related_name = 'comments', on_delete=models.CASCADE)
+    name = models.CharField(max_length=200)
+    body = models.TextField()
+    date_added = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return '%s - %s' % (self.post.title, self.name)
